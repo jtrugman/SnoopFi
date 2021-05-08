a382b59139811c31a76b683e953a798d2526f65d
jtrugman <jtrugman14@gmail.com>
2021-05-08 14:03:41 -0400

    ADD: Sends network data to user via email

--- server/api/controllers/network.js
+++ server/api/controllers/network.js
@@ -1,4 +1,29 @@
-exports.network_update = (req, res, next) => res.status(200).json(
-    {
-        response: req.body.test
-    });
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ee629tm@gmail.com',
+    pass: 'ee629test'
+  }
+});
+
+exports.network_update = (req, res, next) => {
+    var mailOptions = {
+        from: 'ee629tm@gmail.com',
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.net_data
+      };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+    res.status(200).json(
+        {
+            response: "Update Sent"
+        })
+};
